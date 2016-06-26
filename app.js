95d7b81b596a34c001363343dd617707c207c6ae
baluragala <baluragala@gmail.com>
2016-06-26 21:59:23 +0530

    second commit

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.get('/quote', function(req, res){
   res.sendFile(__dirname+'/quote.html');
 });
 
+
+
 var server=app.listen(4001,function(req,res){
     console.log("Catch the action at http://localhost:4000");
 });
